6c04e4e28bb5c02acff21b85b2b0adb102c02a1d
Farinde Abdulrahman <abdulfarinde@gmail.com>
2022-09-22 12:51:21 -0700

    tasks completed

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,27 @@
+#include "main.h"
+#include <string.h>
+
 /**
- * leet - leet
- * @s: string
+ * leet - encodes a string into 1337
+ * @s: the string
  *
- * Return char value 
+ * Return: the encoded string
  */
+
 char *leet(char *s)
 {
-	char alphaArr[] = "a4A4e3E3o0O0t7T7l1L1";
-	int i;
-	int j;
+	int i, j, len;
+	int letters[5] = {97, 101, 111, 116, 108};
+	char num[5] = {'4', '3', '0', '7', '1'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	len = strlen(s);
+
+	for (i = 0; i <  len && s[i] != '\0'; i++)
 	{
-		for (j = 0; alphaArr[j] != '\0'; j++)
+		for (j = 0; j < 5; j++)
 		{
-			if (s[i] == alphaArr[j])
-			{
-				s[i] = alphaArr[j + 1];
-				break;
-			}
+			if (s[i] == letters[j] || s[i] == letters[j] - 32)
+				s[i] = num[j];
 		}
 	}
 	return (s);
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-readme
+6 - More pointers, arrays and strings
